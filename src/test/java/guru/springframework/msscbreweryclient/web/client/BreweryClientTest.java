3500907953964cb76511b01a4c4305b574221066
Diogo Ribeiro de Oliveira <diogo.ribo@gmail.com>
2020-07-18 23:17:31 -0300

    Consuming POST and Get methods from beer

---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.URI;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+
+@SpringBootTest
+class BreweryClientTest {
+
+	@Autowired
+	BreweryClient client;
+
+	@Test
+	void testGetBeerById() {
+		BeerDto beerDto = client.getBeerById(UUID.randomUUID());
+		assertNotNull(beerDto);
+	}
+
+	@Test
+	void testSaveNewBeer() {
+		BeerDto newBeer = BeerDto.builder().beerName("Cacilds").build();
+		URI uri = client.saveNewBeer(newBeer);
+		assertNotNull(uri);
+		System.out.println(uri.toString());
+	}
+
+}
